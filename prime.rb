f7fb91ae201a620a4d0687b0c400f5ef3717f587
Noah  Klem <klem.noah@gmail.com>
2021-05-29 21:05:00 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,9 @@
 # Add  code here!
-def prime?(int)
+def prime?(num)
   if int == 0 || int == 1 || int < 0
     false
   else
-    int.to_a.all? {|i| int % i == 0 }
+    .to_a.all? {|i| int % i == 0 }
   end
 end
   

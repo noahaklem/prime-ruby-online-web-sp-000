d850c4b4bfc29a6883d1c63e50e0ebf3204dc96d
Noah  Klem <klem.noah@gmail.com>
2021-05-29 21:26:27 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+def prime?(num)
+  if num <= 0 || num == 1
+    return false
+  end
+  (2..(num-1)).each do |i| 
+    if num % i == 0
+      return false
+    end
+  end
+  true
+end
+  
+  
